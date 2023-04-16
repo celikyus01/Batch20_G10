3dd96437509745baccd0be09f9fc9684402887d0
celikyus01 <62037357+celikyus01@users.noreply.github.com>
2023-04-16 17:27:57 +0100

    The logging is updated

--- App/src/co/cydeo/b20/LogoutDevelopment.java
+++ App/src/co/cydeo/b20/LogoutDevelopment.java
@@ -3,7 +3,7 @@ package co.cydeo.b20;
 public class LogoutDevelopment {
 
     public static void main(String[] args) {
-        System.out.println("App is logging out, but failed");
+        System.out.println("App is logging out");
     }
 
 
